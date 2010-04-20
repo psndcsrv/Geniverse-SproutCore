a0ace9feb60bffdc31721f9527ebbf6a061a7696
Aaron Unger <aunger@concord.org>
2010-04-20 10:54:39 -0400

    Add a yellow background if the organism is selected.

--- apps/geniverse/views/organism.js
+++ apps/geniverse/views/organism.js
@@ -20,14 +20,26 @@ Geniverse.OrganismView = SC.View.extend(
 		this.set('content', this.get('organism'));
 		this.set('layerNeedsUpdate', YES);
 	}.observes('organism'),
+	
+	isSelectedDidChange: function() {
+		this.set('layerNeedsUpdate', YES);
+	}.observes('isSelected'),
 
   // TODO: This could probably be done cleaner with child views...
   render: function(context) {
-			context.push("<div style='width: 99%; height: 99%; text-align: center;'>" + 
+			context.push("<div style='width: 99%; height: 99%; text-align: center;" + this._selected_style() + "'>" + 
 			   this._image_string() +
 			  '</div>'
 			);
   },
+
+  _selected_style: function() {
+		if (this.get('isSelected') === YES) {
+			return "background-color: yellow;";
+		} else {
+			return "";
+		}
+	},
 
   _image_string: function() {
 		return '<img src="' + this.getPath('content.imageURL') + '" style="width: ' + this.get('layout')['width'] + 'px; height: ' + this.get('layout')['height'] + 'px" />';
