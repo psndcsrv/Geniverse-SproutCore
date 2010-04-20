206170740cfd6b7eb4eef275ca70e62b34cd03c8
Aaron Unger <aunger@concord.org>
2010-04-20 10:02:42 -0400

    GridView of all bred dragons.

--- apps/geniverse/resources/main_page.js
+++ apps/geniverse/resources/main_page.js
@@ -11,15 +11,27 @@ Geniverse.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'breedView listScrollView'.w(),
+    childViews: 'breedView gridScrollView'.w(),
     
     breedView: Geniverse.BreedDragonView.design({
       layout: { centerX: -400, centerY: 0, width: 400, height: 450 }
     }),
 
+		tabView: SC.TabView.design({
+			// value: "Geniverse.mainPage.mainPane.listScrollView",
+			items: [
+			  { title: "List", value: "Geniverse.mainPage.mainPane.listScrollView" },
+				{ title: "Grid", value: "Geniverse.mainPage.mainPane.gridScrollView" }
+			],
+			itemTitleKey: 'title',
+      itemValueKey: 'value',
+			layout: { centerX: 100, centerY: 0, width: 620, height: 480 },
+			userDefaultKey: "mainPanel"
+		}),
+		
 		listScrollView: SC.ScrollView.design({
 		  hasHorizontalScroller: NO,
-      layout: { centerX: 100, centerY: 0, width: 600, height: 450 },
+      layout: { left: 0, top: 0, width: 600, height: 450 },
       backgroundColor: 'white',
       contentView: SC.ListView.design({
 				contentBinding: 'Geniverse.bredOrganismsController.arrangedObjects',
@@ -28,8 +40,22 @@ Geniverse.mainPage = SC.Page.design({
 				canEditContent: NO,
 				contentIconKey: 'imageURL',
 				hasContentIcon: YES,
-				contentValueKey: 'info',
-				escapeHTML: NO
+				contentValueKey: 'info'
+      })
+    }),
+
+		gridScrollView: SC.ScrollView.design({
+		  hasHorizontalScroller: NO,
+      // layout: { left: 0, top: 0, width: 600, height: 450 },
+			layout: { centerX: 100, centerY: 0, width: 620, height: 480 },
+      backgroundColor: 'white',
+      contentView: SC.GridView.design({
+				contentBinding: 'Geniverse.bredOrganismsController.arrangedObjects',
+				selectionBinding: 'Geniverse.bredOrganismsController.selection',
+				rowHeight: 60,
+				columnWidth: 60,
+				canEditContent: NO,
+				exampleView: Geniverse.OrganismView
       })
     })
   })
--- apps/geniverse/views/breed_dragon.js
+++ apps/geniverse/views/breed_dragon.js
@@ -45,28 +45,40 @@ Geniverse.BreedDragonView = SC.View.extend(
   father: null,
   child: null,
 
-	childViews: 'motherView fatherView childView breedButtonView'.w(),
+	childViews: 'motherView fatherView childView motherLabel fatherLabel childLabel breedButtonView'.w(),
 	
 	fatherView: Geniverse.OrganismView.design({
-	  layout: { right: 0, top: 0, width: 188, height: 141 },
-	  organismBinding: "*parentView.father",
-	  label: "Father"
+	  layout: { left: 188, top: 18, width: 188, height: 141 },
+	  organismBinding: "*parentView.father"
+	}),
+	
+	fatherLabel: SC.LabelView.design({
+		layout: { left: 247, top: 0, width: 50, height: 18 },
+		value: "Father"
 	}),
 	
 	motherView: Geniverse.OrganismView.design({
-	  layout: { left: 0, top: 0, width: 188, height: 141 },
-	  organismBinding: "*parentView.mother",
-	  label: "Mother"
+	  layout: { left: 0, top: 18, width: 188, height: 141 },
+	  organismBinding: "*parentView.mother"
+	}),
+	
+	motherLabel: SC.LabelView.design({
+		layout: { left: 59, top: 0, width: 50, height: 18 },
+		value: "Mother"
 	}),
 	
 	childView: Geniverse.OrganismView.design({
-	  layout: { centerX: 0, top: 141, width: 188, height: 141 },
-	  organismBinding: "*parentView.child",
-	  label: "Child"
+	  layout: { centerX: 0, top: 159, width: 188, height: 141 },
+	  organismBinding: "*parentView.child"
+	}),
+	
+	childLabel: SC.LabelView.design({
+		layout: { centerX: 0, top: 147, width: 40, height: 18 },
+		value: "Child"
 	}),
 	
 	breedButtonView: SC.ButtonView.design({
-		layout: { centerX: 0, top: 282, width: 100, height: 24 },
+		layout: { centerX: 0, top: 288, width: 100, height: 24 },
 		action: "this.parentView.buttonAction",
 		title: "Breed"
 	}),
