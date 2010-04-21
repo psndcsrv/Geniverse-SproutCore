05787253626e5caa48b7b1a93bb48139b24a1c57
Aaron Unger <aunger@concord.org>
2010-04-21 11:10:33 -0400

    Better fixtures.

--- apps/geniverse/fixtures/dragon.js
+++ apps/geniverse/fixtures/dragon.js
@@ -8,28 +8,62 @@ sc_require('models/dragon');
 
 Geniverse.Dragon.FIXTURES = [
 
-  // TODO: Add your data fixtures here.
-  // All fixture records must have a unique primary key (default 'guid').  See 
-  // the example below.
-
-  // { guid: 1,
-  //   firstName: "Michael",
-  //   lastName: "Scott" },
-  //
-  // { guid: 2,
-  //   firstName: "Dwight",
-  //   lastName: "Schrute" },
-  //
-  // { guid: 3,
-  //   firstName: "Jim",
-  //   lastName: "Halpert" },
-  //
-  // { guid: 4,
-  //   firstName: "Pam",
-  //   lastName: "Beesly" },
-  //
-  // { guid: 5,
-  //   firstName: "Ryan",
-  //   lastName: "Howard" }
+// Add your data fixtures here.
+// All fixture records must have a unique primary key (default 'guid').  See
+// the example below.
+// {
+//     guid: 1,
+//     sex: 0,
+//     alleles: "hh,SS,WW,lL,tt,pp,FF,aa,BB",
+//     bred: true,
+//     imageURL: "/geniverse/cache/hh,SS,WW,lL,tt,pp,FF,aa,BB.png",
+//     characteristics: ['Fill','In','Characteristics'],
+//     metaInfo: {}
+// },
+// {
+//     guid: 2,
+//     sex: 1,
+//     alleles: "hh,SS,Ww,lL,TT,pp,FF,AA,BB",
+// 		bred: true,
+//     imageURL: "/geniverse/cache/hh,SS,Ww,lL,TT,pp,FF,AA,BB.png",
+//     characteristics: ['Fill','In','Characteristics'],
+//     metaInfo: {}
+// },
+// {
+//     guid: 3,
+//     sex: 0,
+//     alleles: "hh,SS,Ww,lL,tT,pp,FF,aA,BB",
+// 		bred: true,
+//     imageURL: "/geniverse/cache/hh,SS,Ww,lL,tT,pp,FF,aA,BB.png",
+//     characteristics: ['Fill','In','Characteristics'],
+//     metaInfo: {}
+// },
+// {
+//     guid: 4,
+//     sex: 1,
+//     alleles: "hh,SS,wW,ll,tt,pp,ff,aa,bB",
+// 		bred: true,
+//     imageURL: "/geniverse/cache/hh,SS,wW,ll,tt,pp,ff,aa,bB.png",
+//     characteristics: ['Fill','In','Characteristics'],
+//     metaInfo: {}
+// },
+// {
+//     guid: 5,
+//     sex: 0,
+//     alleles: "Hh,SS,Ww,ll,TT,pp,FF,aa,BB",
+// 		bred: true,
+//     imageURL: "/geniverse/cache/Hh,SS,Ww,ll,TT,pp,FF,aa,BB.png",
+//     characteristics: ['Fill','In','Characteristics'],
+//     metaInfo: {}
+// },
+// {
+//     guid: 6,
+//     sex: 0,
+//     alleles: "hH,SS,WW,ll,tT,pp,ff,aa,bb",
+// 		bred: true,
+//     imageURL: "/geniverse/cache/hH,SS,WW,ll,tT,pp,ff,aa,bb.png",
+//     characteristics: ['Fill','In','Characteristics'],
+//     metaInfo: {}
+// }
 
 ];
