c47bce336c6e927cbadfcacb20132149af774a1e
Aaron Unger <aunger@concord.org>
2010-08-13 10:15:08 -0400

    Fix class name definition in Dragon relationships

--- rails/geniverse/app/models/dragon.rb
+++ rails/geniverse/app/models/dragon.rb
@@ -1,6 +1,6 @@
 class Dragon < ActiveRecord::Base
-  has_many :children, :class_name => Dragon, :finder_sql => 'SELECT * FROM dragons WHERE mother_id = #{id} OR father_id = #{id}'
+  has_many :children, :class_name => "Dragon", :finder_sql => 'SELECT * FROM dragons WHERE mother_id = #{id} OR father_id = #{id}'
   
-  belongs_to :father, :class_name => Dragon
-  belongs_to :mother, :class_name => Dragon
+  belongs_to :father, :class_name => "Dragon"
+  belongs_to :mother, :class_name => "Dragon"
 end
