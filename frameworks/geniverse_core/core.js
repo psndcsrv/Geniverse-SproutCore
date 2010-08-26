70d7f8ad0f144bea139dc78db265555e08890c5a
Aaron Unger <aunger@concord.org>
2010-08-26 12:02:10 -0400

    Initial start of a GeniverseCore framework, into which we can start moving non-specific Geniverse views, models and controllers.

---
+++ frameworks/geniverse_core/core.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Project:   GeniverseCore
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals GeniverseCore */
+
+/** @namespace
+
+  My cool new framework.  Describe your framework.
+  
+  @extends SC.Object
+*/
+GeniverseCore = SC.Object.create(
+  /** @scope GeniverseCore.prototype */ {
+
+  NAMESPACE: 'GeniverseCore',
+  VERSION: '0.1.0',
+
+  // TODO: Add global constants or singleton objects needed by your app here.
+
+}) ;
